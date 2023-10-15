e0481a63c2c636f48f89a0a40032d9cca6a49266
Said Said <ssaid20@icloud.com>
2023-10-15 16:15:09 -0500

    updated shadcn ui

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { Button } from "../ui/button";
+// import { Checkbox } from "@/components/ui/checkbox";
+import { Checkbox } from "../ui/checkbox";
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -12,6 +14,9 @@ function AboutPage() {
         This about page is for anyone to read!
       </h1>
       <div className="panel">
+        <Button variant="outline">Button</Button>
+        <Checkbox />
+
         <p>This about page is for anyone to read!</p>
       </div>
     </div>
