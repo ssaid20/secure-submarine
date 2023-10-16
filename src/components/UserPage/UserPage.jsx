b93643174f098c88f1857346792060b63027f86b
Said Said <ssaid20@icloud.com>
2023-10-16 08:29:43 -0500

    added a card component from shadcn ui

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
-
+import CardWithForm  from '../CardWithForm/CardWithForm';
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
-    <div className="container">
-      {/* <div className="panel">
-        <h2 className="titleBase_inner">Welcome, {user.username}!</h2>
-        <p>Your ID is: {user.id}</p>
-        <LogOutButton className="btn" />
-      </div> */}
-    </div>
+   
+      
+      <main className="w-full h-full bg-background flex min-h-screen flex-col items-center">
+        <div className="w-full flex justify-end p-4">
+          {/* <ModeToggle /> */}
+        </div>
+
+        
+        <div className="flex min-h-screen items-center">
+          <CardWithForm />
+        </div>
+      </main>
+      // {/* <div className="panel">
+      //   <h2 className="titleBase_inner">Welcome, {user.username}!</h2>
+      //   <p>Your ID is: {user.id}</p>
+      //   <LogOutButton className="btn" />
+      // </div> */}
+    
   );
 }
 
